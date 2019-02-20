395f259f3b33b296a19e3a06a040feb266b02a1e
David <47166040+FootlessCat985@users.noreply.github.com>
2019-02-20 08:36:59 -0700

    salto y coliciones
    se agregaron coliciones y un salto

---
+++ PhysicMaterials_Addforce/Assets/scripst/Boxphysics.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boxphysics : MonoBehaviour
+{
+    Rigidbody2D rb2D;
+
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+        rb2D.AddForce(Vector2.up * 500, ForceMode2D.Force);
+    }
+}
